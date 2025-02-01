fa0ba0ae453a5790b4512d9287d31748d170078b
Kuzdra24 <patryk.kuzdrowski@gmail.com>
2025-02-01 14:22:27 +0100

    feat: add property details table

--- app/oferta/[slug]/page.tsx
+++ app/oferta/[slug]/page.tsx
@@ -1,8 +1,9 @@
-
+import { DollarSign, Home, Bed, Grid2X2 } from 'lucide-react';
 import { useRouter } from 'next/router';
 import { ParsedUrlQuery } from 'querystring';
 import Image from 'next/image';
 import mockOfferData from "@/data/mockOfferData.json";
+import { Button } from '@/components/UI/Button';
 
 interface Offer {
   id: string;
@@ -32,7 +33,6 @@ export default async function Page({
 }) {
   const { slug } = params;
 
-  // Fetch offer data based on slug
   const offer = mockOfferData.find(el => el.id === slug);
 
   if (!offer) {
@@ -42,17 +42,48 @@ export default async function Page({
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-4xl font-bold mb-4">{offer.title}</h1>
       <p className="text-gray-500 text-sm mb-4">{offer.location}</p>
-      <img src={offer.imageUrl} width={650} height={500} alt={offer.title} className="w-full h-auto rounded-md mb-4" />
-      <p className="text-lg mb-4">{offer.description}</p>
+      <div className="flex items-center">
+
+      <Image src={offer.imageUrl} width={650} height={500} alt={offer.title} className="w-full max-w-[50%] h-auto rounded-md mb-4" />
+      <div className="flex w-[50%] flex-col items-center justify-center">
+      <table className="w-full lg:w-auto border-collapse mb-[20px]">
+          <tbody className='flex flex-col'>
+            <tr className="border-b flex">
+              <td className="p-2 font-semibold flex items-center justify-center">
+                <DollarSign className="w-4 h-4 mr-1" /> Cena:
+              </td>
+              <td className="p-2">{offer.price} PLN</td>
+            </tr>
+            <tr className="border-b flex">
+              <td className="p-2 font-semibold flex items-center justify-center">
+                <Grid2X2 className="w-4 h-4 mr-1" /> Powierzchnia:
+              </td>
+              <td className="p-2">{offer.area} m²</td>
+            </tr>
+            <tr className="border-b flex">
+              <td className="p-2 font-semibold flex items-center justify-center">
+                <Home className="w-4 h-4 mr-1" /> Typ oferty:
+              </td>
+              <td className="p-2">{offer.type}</td>
+            </tr>
+            <tr className="border-b flex">
+              <td className="p-2 font-semibold flex items-center justify-center">
+                <Bed className="w-4 h-4 mr-1" /> Pokoje:
+              </td>
+              <td className="p-2">{offer.rooms}</td>
+            </tr>
+          </tbody>
+        </table>
+        <Button variant={"primary"}>Zapytaj o ofertę</Button>
+      </div>
+      </div>
+      <p className="text-lg mb-4 max-w-[70%]">{offer.description}</p>
       <ul className="list-disc list-inside mb-4">
         {offer.features.map((feature, index) => (
           <li key={index}>{feature}</li>
         ))}
       </ul>
-      <p className="text-lg font-semibold">Cena: {offer.price} PLN</p>
-      <p className="text-lg font-semibold">Powierzchnia: {offer.area} m²</p>
-      <p className="text-lg font-semibold">Liczba pokoi: {offer.rooms}</p>
-      <p className="text-lg font-semibold">Typ: {offer.type}</p>
+    
     </div>
   );
 };
